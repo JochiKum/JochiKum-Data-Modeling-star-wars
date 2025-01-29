ee67babf1c5272276b52b7fa032903d47d17c480
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2025-01-29 16:13:05 +0000

    Modelado de starwars

--- src/models.py
+++ src/models.py
@@ -7,23 +7,47 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)  
+    name = Column(String(250), nullable=False, unique=True)
+    last_name = Column (String(250), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    biography = Column (String(200), nullable=False, unique=False)
+    password = Column(String(50), nullable=False, unique=False)
+    favorites = relationship("Favorite", back_populates="user")
 
-class Address(Base):
-    __tablename__ = 'address'
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(100), nullable=False, unique=True)
+    description = Column(String(500), nullable=False)
+    favorites = relationship("Favorite", back_populates="character")
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(150), unique=True, nullable=False)
+    description = Column(String(500), nullable=False)
+    favorites = relationship("Favorite", back_populates="planet") 
+
+class Favorite(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    user = relationship("User", back_populates="favorites")
+    character = relationship("Character", back_populates="favorites")
+    planet = relationship("Planet", back_populates="favorites")       
 
     def to_dict(self):
         return {}
